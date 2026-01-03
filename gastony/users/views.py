334aa106452935223b63be7820a7b5af7d06b338
Brian <brian95gsus@gmail.com>
2026-01-02 23:43:22 -0300

    feat: add user crud and login

--- gastony/users/views.py
+++ gastony/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet): 
+    queryset=User.objects.all()
+    permission_classes=[permissions.AllowAny]
+
+
+    serializer_class = UserSerializer
