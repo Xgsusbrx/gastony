af8e96d8e98e9b234b10b3f2697d1d1c2a834d3e
Brian <brian95gsus@gmail.com>
2026-01-09 17:24:59 -0300

    add modulo de analisis del texto con ia

--- gastony/accounting/utils.py
+++ gastony/accounting/utils.py
@@ -5,6 +5,7 @@ def ocr(image):
 
     from PIL import Image
     import pytesseract
+    
 
 
     pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Usuario\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
@@ -15,6 +16,12 @@ def ocr(image):
     # Extraer texto
     texto = pytesseract.image_to_string(imagen, lang="spa")
     return texto
+    # funcion que se encarga de consultar con la ia 
+    ollama_client = ollama.Client()
+
+    ollama_model = ollama_client.load_model('llama3')
+
+   
 
     # Guardar en archivo txt
     # with open("texto_extraido.txt", "w", encoding="utf-8") as archivo:
--- gastony/accounting/views.py
+++ gastony/accounting/views.py
@@ -6,6 +6,9 @@ from rest_framework.decorators import action
 from .models import Transaction
 from .serializers import OcrSerializer 
 from .utils import ocr
+import requests 
+
+
 
 
 
@@ -26,10 +29,58 @@ class OcrViewSet(viewsets.ModelViewSet):
         serializer = OcrSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         text=ocr(serializer.validated_data.get('image'))
-        Transaction.objects.create(notes=text,amount=0)
+        text_test="Hola acabo de gastar 5000 pesos en un cafe"
+        body={
+            "model": "gemma3",
+            "prompt": f"A partir del siguiente texto extraído por OCR devuélvelo ordenado y estructurado en formato JSON. Devuelve SOLO JSON válido, sin texto adicional solo con estos campos: - nombre    - fecha- monto - concepto    Texto OCR: {text_test}",
+            "stream": False
+             }
+        resolve=requests.post("http://localhost:11434/api/generate",json=body)
+        import json
+        import re
         
+        test=json.loads(resolve.text)
+        result=test["response"]
+        print(test["response"])
+        coincide=re.search(r"```json\s*(\{\s*.*?\s*\})\s*```",result,re.DOTALL)
+
+        if coincide:
+            json_srt= coincide.group(1)
+            try:
+                data = json.loads(json_srt)
+                print(data)
+                print("Nombre del primer elemento")
+            except json.JSONDecodeError as e:
+                print("Error parseando JSON ",e)
+        else:
+            print("No se encontro JSON")            
         
-        data={"texto_de_la_imagen":text}
+
+    #     formateado = ollama(f"""
+    # A partir del siguiente texto extraído por OCR,
+    # devuélvelo ordenado y estructurado en formato JSON.
+
+    # Devuelve SOLO JSON válido, sin texto adicional.
+
+    # Campos sugeridos:
+    # - nombre
+    # - fecha
+    # - monto
+    # - concepto
+
+    # Texto OCR:
+    # {text}
+    # """)
+        
+        import decimal
+        concepto=data.get("concepto") 
+        monto=data.get("monto")
+        monto=decimal.Decimal(monto)
+        
+        Transaction.objects.create(notes=concepto,amount=monto)
+
+        
+        # data={"tu resumen es ": text,"resolve":test["response"]}
         return Response(data)
     
     
